5d7bae7a1b65c99858f075123b18bef4dc419400
orangejamface <orangejamface@gmail.com>
2022-03-01 13:08:38 -0500

    added new video, cleaned up styling, removed commented out index sections

--- java.js
+++ java.js
@@ -1,9 +1,18 @@
 
-
+// three variable below correspond to large sections, each with the same styling.
 
   const sound = document.getElementById("soundDesign");
   const video = document.getElementById("video");
   const music = document.getElementById("music");
+
+  // Three functions below in order to toggle between showing and hiding those three different sections.
+  // Each section has the same structure and is reached by clicking its corresponding button
+  // currently Sound Dsign / Composition is the default upon load and you can click Videogrpahy or music to
+  // show that section and hide the others.
+  // one issue however is if oyu clikc the button of the section you are currently on it will hide it, meaning
+  // all sections will be hidden and the website will be essentially blank except fo the header and footer.
+  // I could not figure out how to stop this, it might mean ap[proaching the problem differntly, this is 
+  // and improvment I would like to work on at some point in the future.
 
 function toggleSound() {
   if (sound.style.display !== "flex") {
@@ -35,6 +44,3 @@ function toggleMusic() {
       music.style.display = "none";
       }
 }
-
-
-// BELOW APPLIES TO VIDEO LOADING AND PLAYING
